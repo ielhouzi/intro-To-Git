5679e0b62d68e6e781114df3f639aea147739c75
snir2g1 <snir2g1@localhost>
2022-01-20 16:35:15 +0100

    Ajout d'un message "yo"

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Bonjour" << endl;
+    cout<<"Tciao"<<endl;
+    cout<<"c'est encore moi"<<endl;
+    return 0;
+}
+
